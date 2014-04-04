c0cd5160de2d84db90ac1f9c2fe4533a55e94d07
Paweł Ksieniewicz <pawel@ksienie.com>
2014-04-04 14:52:13 +0200

    One—file.

--- Promyk/Promyk.m
+++ Promyk/Promyk.m
@@ -1,12 +1,12 @@
 //
-//  DOViewController.m
 //  Promyk
 //
 //  Created by Paweł Ksieniewicz on 04.04.2014.
 //  Copyright (c) 2014 Deathly Owl. All rights reserved.
 //
 
-#import "DOViewController.h"
+#import <Availability.h>
+#import <UIKit/UIKit.h>
 
 #define LINE_WIDTH 8
 #define SMALL_UNIT 20
@@ -14,6 +14,58 @@
 #define DASH_PATTERN @[@LINE_WIDTH,@LINE_WIDTH]
 #define ANIMATION_DURATION 1
 #define YELLOW [UIColor colorWithHue:0.16f saturation:0.51f brightness:1.00f alpha:1.00f]
+#define FONT [UIFont fontWithName:@"ModernSans" size:50]
+
+@interface DOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
+@interface DOViewController : UIViewController
+
+@end
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DOAppDelegate class]));
+    }
+}
+
+@implementation DOAppDelegate
+
+@synthesize window;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [application setStatusBarHidden:YES];
+    
+    application.idleTimerDisabled = YES;    // Do not fall asleep
+    
+    // Generate and show view
+    window = UIWindow.new;
+    window.frame = UIScreen.mainScreen.bounds;
+    window.rootViewController = DOViewController.new;
+    
+    [window setClipsToBounds:YES];
+    
+    // Initialize location manager
+    /*
+     locationManager = CLLocationManager.new;
+     
+     locationManager.delegate = self;
+     locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+     
+     [self.window makeKeyAndVisible];
+     [self locate];
+     */
+    
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
 
 @interface DOViewController ()
 {
@@ -33,27 +85,6 @@ double radiansToDegrees(double radians){return radians * 180 / M_PI;}
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self configure];
-    
-    hourLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 2*SMALL_UNIT, 320, 2.5*SMALL_UNIT)];
-    [hourLabel setFont:[UIFont fontWithName:@"ModernSans"
-                                   size:50]];
-    [hourLabel setTextAlignment:NSTextAlignmentCenter];
-    [hourLabel.layer setOpacity:.1];
-    [self.view addSubview:hourLabel];
-    
-    verbumLabel = [[UILabel alloc] initWithFrame:CGRectMake(SMALL_UNIT, 5*BIG_UNIT, 320 - 2*SMALL_UNIT, 2.5*SMALL_UNIT)];
-    [verbumLabel setFont:[UIFont fontWithName:@"ModernSans"
-                                         size:50]];
-    [verbumLabel setTextAlignment:NSTextAlignmentCenter];
-    [verbumLabel setMinimumScaleFactor:.1];
-    [verbumLabel setAdjustsFontSizeToFitWidth:YES];
-    [self.view addSubview:verbumLabel];
-    
-    angleLabel = [[UILabel alloc] initWithFrame:CGRectMake(BIG_UNIT+10, 3*BIG_UNIT + SMALL_UNIT, 2*BIG_UNIT, 2.5*SMALL_UNIT)];
-    [angleLabel setFont:[UIFont fontWithName:@"ModernSans"
-                                        size:50]];
-    [angleLabel setTextAlignment:NSTextAlignmentCenter];
-    [self.view addSubview:angleLabel];
 }
 
 - (void) tick
@@ -65,9 +96,8 @@ double radiansToDegrees(double radians){return radians * 180 / M_PI;}
     NSInteger minute = [components minute];
     NSInteger second = [components second];
     
-    
     NSString *separator = second % 2 ? @":" : @".";
- 
+    
     [hourLabel setText:[NSString stringWithFormat:@"%i%@%02i", hour, separator, minute]];
     
     [self setSeconds:second];
@@ -109,12 +139,11 @@ double radiansToDegrees(double radians){return radians * 180 / M_PI;}
     [handDashed setPosition:self.view.center];
     [handDashed setStrokeColor:[UIColor blackColor].CGColor];
     [handDashed setLineWidth:LINE_WIDTH];
+    [handDashed setLineDashPattern:DASH_PATTERN];
     
     path = CGPathCreateMutable();
     CGPathMoveToPoint(path, NULL, 0, 0);
     CGPathAddLineToPoint(path, NULL, -6 * SMALL_UNIT, 0);
-    
-    [handDashed setLineDashPattern:DASH_PATTERN];
     
     [handDashed setPath:path];
     
@@ -125,12 +154,10 @@ double radiansToDegrees(double radians){return radians * 180 / M_PI;}
     [innerCircle setFillColor:[UIColor clearColor].CGColor];
     [innerCircle setLineWidth:LINE_WIDTH];
     [innerCircle setLineCap:kCALineCapRound];
-    
     [innerCircle setPath:[UIBezierPath bezierPathWithOvalInRect:CGRectMake(-SMALL_UNIT * 6,
                                                                            -SMALL_UNIT * 6,
                                                                            SMALL_UNIT * 12,
                                                                            SMALL_UNIT * 12)].CGPath];
-    
     
     // Outer circle
     outerCircle = [CAShapeLayer layer];
@@ -142,7 +169,6 @@ double radiansToDegrees(double radians){return radians * 180 / M_PI;}
     [outerCircle setStrokeStart:0];
     [outerCircle setStrokeEnd:0];
     [outerCircle setTransform:CATransform3DMakeRotation(-M_PI_2, 0, 0, 1)];
-    
     [outerCircle setPath:[UIBezierPath bezierPathWithOvalInRect:CGRectMake(-SMALL_UNIT * 7,
                                                                            -SMALL_UNIT * 7,
                                                                            SMALL_UNIT * 14,
@@ -157,6 +183,20 @@ double radiansToDegrees(double radians){return radians * 180 / M_PI;}
     [sun setPath:[UIBezierPath bezierPathWithOvalInRect:CGRectMake(-SMALL_UNIT*6 -LINE_WIDTH,-LINE_WIDTH,
                                                                    LINE_WIDTH*2,
                                                                    LINE_WIDTH*2)].CGPath];
+    
+    // Labels
+    hourLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 2*SMALL_UNIT, 320, 2.5*SMALL_UNIT)];
+    verbumLabel = [[UILabel alloc] initWithFrame:CGRectMake(SMALL_UNIT, 5*BIG_UNIT, 320 - 2*SMALL_UNIT, 2.5*SMALL_UNIT)];
+    angleLabel = [[UILabel alloc] initWithFrame:CGRectMake(BIG_UNIT+10, 3*BIG_UNIT + SMALL_UNIT, 2*BIG_UNIT, 2.5*SMALL_UNIT)];
+    
+    hourLabel.font = verbumLabel.font = angleLabel.font = FONT;
+    hourLabel.textAlignment = verbumLabel.textAlignment = angleLabel.textAlignment = NSTextAlignmentCenter;
+    
+    [hourLabel.layer setOpacity:.1];
+    
+    [verbumLabel setMinimumScaleFactor:.1];
+    [verbumLabel setAdjustsFontSizeToFitWidth:YES];
+    
     // Place all the layers
     [self.view.layer addSublayer:outerCircle];
     [self.view.layer addSublayer:innerCircle];
@@ -164,24 +204,22 @@ double radiansToDegrees(double radians){return radians * 180 / M_PI;}
     [self.view.layer addSublayer:handSolid];
     [self.view.layer addSublayer:handDashed];
     [handDashed addSublayer:sun];
+    [self.view addSubview:verbumLabel];
+    [self.view addSubview:hourLabel];
+    [self.view addSubview:angleLabel];
 }
 
-- (void)viewDidAppear:(BOOL)animated{
+- (void)viewDidAppear:(BOOL)animated
+{
     [super viewDidAppear:YES];
     [self setAngle];
-    
-    [[NSTimer scheduledTimerWithTimeInterval:1
-                                      target:self
-                                    selector:@selector(tick)
-                                    userInfo:nil
-                                     repeats:YES] fire];
+    [[NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(tick) userInfo:nil repeats:YES] fire];
 }
 
 - (void) setSeconds:(int) seconds
 {
     [outerCircle setStrokeStart:0];
     [outerCircle setStrokeEnd:(float)seconds/59.];
-    
     if (seconds == 0) [self setAngle];
 }
 
@@ -203,7 +241,7 @@ double radiansToDegrees(double radians){return radians * 180 / M_PI;}
     
     [verbumLabel setText:@"twilight in 15"];
     
-    float minAngle = -45;
+    float minAngle = -35;
     float maxAngle = 45;
     
     float minC = minAngle / 360;
@@ -211,7 +249,6 @@ double radiansToDegrees(double radians){return radians * 180 / M_PI;}
     
     [innerCircle setStrokeStart:.5 + minC];
     [innerCircle setStrokeEnd:.5 + maxC];
-    
 }
 
 @end
